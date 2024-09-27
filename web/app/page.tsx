2de6b2ee5b0f6dc54751f17b000b649b83bd0049
Zhiao Mo <mozhiao@iCloud.com>
2024-09-27 22:44:06 +1000

    f

---
+++ web/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { useState } from "react";
+import Cookies from 'js-cookie';
+
+export default function Home() {
+
+  return (
+    <main className="flex flex-wrap h-dvh w-screen text-black">bilibili</main>
+  );
+}
